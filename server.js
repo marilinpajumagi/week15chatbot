2e9425f9b3b5ee97ce48b93ab28721318db0b0b4
Marilin Pajumägi <marilin.pajumagi@tktk.ee>
2024-05-30 13:03:02 +0300

    is it fixed now

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-
+const { default: axios } = require('axios');
 const express = require('express');
-const axios = require('axios');
+const http = require('http');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
+
+    
     let url = 'https://api.themoviedb.org/3/movie/550988?api_key=4ca94f8b470d7e34bd3f59c3914295c8';
     axios.get(url)
     .then(response => {
+        console.log(response.data.title);
         let data = response.data;
         let releaseDate = new Date(data.release_date).getFullYear();
         let genres = '';
@@ -19,47 +23,62 @@ app.get('/', (req, res) => {
             genres = genres + `${genre.name}, `;
         });
 
+        
         let genresUpdated = genres.slice(0, -2) + '.';
-        let moviePoster = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+        moviePoster = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+        console.log(genresUpdated);
         let currentYear = new Date().getFullYear();
         res.render('index', {movieData: data, releaseDate: releaseDate, genres: genresUpdated, poster: moviePoster, year: currentYear});
-    }).catch(error => {
-        console.error('Error fetching movie data:', error);
-        res.status(500).send('Error fetching movie data');
     });
+    
 });
 
-// Modified /getmovie route using axios
+//new route
 app.post('/getmovie', (req, res) => {
-    const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : '';
+	const movieToSearch =
+		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
+			? req.body.queryResult.parameters.movie
+			: '';
 
-    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=d5bdccf2`);
+	const reqUrl = encodeURI(
+		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=d5bdccf2`
+	);
+	http.get(
+		reqUrl,
+		responseFromAPI => {
+			let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+			responseFromAPI.on('end', () => {
+				const movie = JSON.parse(completeResponse);
+                if (!movie || !movie.Title) {
+                    return res.json({
+                        fulfillmentText: 'Sorry, we could not find the movie you are asking for.',
+                        source: 'getmovie'
+                    });
+                }
 
-    axios.get(reqUrl)
-    .then(response => {
-        const movie = response.data;
-        if (!movie || !movie.Title) {
-            return res.json({
-                fulfillmentText: 'Sorry, we could not find the movie you are asking for.',
-                source: 'getmovie'
-            });
-        }
+				let dataToSend = movieToSearch;
+				dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
+					movie.Director
+				} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.`;
 
-        const dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${movie.Director} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.`;
-
-        return res.json({
-            fulfillmentText: dataToSend,
-            source: 'getmovie'
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching movie data:', error);
-        return res.json({
-            fulfillmentText: 'Could not get results at this time',
-            source: 'getmovie'
-        });
-    });
+				return res.json({
+					fulfillmentText: dataToSend,
+					source: 'getmovie'
+				});
+			})
+		},
+		error => {
+			return res.json({
+				fulfillmentText: 'Could not get results at this time',
+				source: 'getmovie'
+			});
+		}
+	)
 });
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('server is running');
